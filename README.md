e8a95e3618bb36a4f0700d021049d5f08b7f2744
digitalyoshixi <digitalyoshixi@proton.me>
2025-12-06 17:50:53 -0500

    feat: initial variables

---
+++ README.md
@@ -0,0 +1,7 @@
+# CHIP-8 Emulator
+Referenced from: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
+
+# Compilation Command
+```
+g++ -o ./emulator -g emulator.cpp
+```
---
+++ emulator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "./include/imgui.h"
+#include <stack>
+
+int main(){
+  short program_memory[4096]; // ROM
+  bool display[64][32]; // 64 x 32 display
+  int pc; // program counter register (PC)
+  int ic; // index register (I)
+  short registers[16]; // V0 - VF
+  std::stack<short> st; // stack
+  short delay_timer = 60;
+  short sound_timer = 60;
+  return 0;
+}
